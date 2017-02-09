1df314b3d170b40057c0c09c3aa40811a218c50b
Andrew Bredow <bredow@gmail.com>
2017-02-09 17:30:04 -0500

    Randomize cause order on page load

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var causes    = Array.prototype.slice.call(document.querySelectorAll(".cause"), 0);
+  var container = document.querySelector(".causes-wrapper");
+
+  var sortedCauses = causes.sort(function() {
+    return Math.random() > 0.5 ? -1 : 1;
+  });
+
+  for (cause in sortedCauses) {
+    container.appendChild(sortedCauses[cause]);
+  }
+
   document.querySelectorAll("[data-select-on-click]").forEach(function(el) {
     el.onclick = function() {
       this.select();
